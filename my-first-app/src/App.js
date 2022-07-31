af69a0ad04a481a7b25d25e58af4b09c45b34309
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2022-07-31 12:25:46 +0500

    Imported the Moviecard Component

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,24 +1,38 @@
 import { getByTestId } from "@testing-library/react";
 import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 // bd513f5d
 
 const API_URL = "http://www.omdbapi.com?apikey=bd513f5d";
 const App = () => {
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-        console.log(data.Search);
-    };
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
 
-    useEffect(() => {
-        searchMovies("Spiderman");
-    });
-    return (
-        <>
-            <h1>Abdul Aziz</h1>
-        </>
-    );
+  useEffect(() => {
+    searchMovies("Spiderman");
+  });
+  return (
+    <>
+      <div className="app">
+        <h1>Movie Land</h1>
+
+        <div className="search">
+          <input placeholder="Search for movies" value="" onChange={() => {}} />
+          <img src={SearchIcon} alt="search" onClick={() => {}} />
+        </div>
+
+        <div className="container">
+          <MovieCard />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
