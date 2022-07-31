764d067b14eac83ac27006094b6dfd637c2a5a05
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2022-07-31 12:59:17 +0500

    Completed basic react app..

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,5 +1,4 @@
-import { getByTestId } from "@testing-library/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
@@ -7,31 +6,48 @@ import MovieCard from "./MovieCard";
 // bd513f5d
 
 const API_URL = "http://www.omdbapi.com?apikey=bd513f5d";
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
     searchMovies("Spiderman");
-  });
+  }, []);
   return (
-    <>
-      <div className="app">
-        <h1>Movie Land</h1>
+    <div className="app">
+      <h1>Movie Land</h1>
 
-        <div className="search">
-          <input placeholder="Search for movies" value="" onChange={() => {}} />
-          <img src={SearchIcon} alt="search" onClick={() => {}} />
-        </div>
-
-        <div className="container">
-          <MovieCard />
-        </div>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
-    </>
+
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie1={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies found</h2>
+        </div>
+      )}
+    </div>
   );
 };
 
--- my-first-app/src/MovieCard.jsx
+++ my-first-app/src/MovieCard.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 
-const movie1 = {
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
-  Title: "Italian Spiderman",
-  Type: "movie",
-  Year: "2007",
-  imdbID: "tt2705436",
-};
-
-const MovieCard = () => {
+const MovieCard = ({ movie1 }) => {
   return (
     <div className="movie">
       <div>
         <p>{movie1.Year}</p>
       </div>
       <div>
-        <img src={movie1.Poster} alt="poster" />
+        <img
+          src={
+            movie1.Poster !== "N/A"
+              ? movie1.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt="poster"
+        />
       </div>
       <div>
         <span>{movie1.Type}</span>
